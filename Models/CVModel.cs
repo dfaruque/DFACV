73eea67f8fc55bc73414d86b21037eefdcd70e4c
D Faruque Ahmad <dfaruque@gmail.com>
2022-10-01 19:05:21 +0600

    added Hobby Projects

--- Models/CVModel.cs
+++ Models/CVModel.cs
@@ -16,6 +16,7 @@ namespace DFACV.Models
         public List<ExperienceModel> Experiences { get; set; }
         public List<EducationModel> Educations { get; set; }
         public List<ProjectModel> Projects { get; set; }
+        public List<ProjectModel> HobbyProjects { get; set; }
         public List<AwardModel> Awards { get; set; }
         public List<ContactModel> Contacts { get; set; }
         public List<SocialMediaModel> SocialMedias { get; set; }
@@ -522,6 +523,67 @@ https://www.nuget.org/packages/Serenity.Extra
 Beginning programming with VB6 at school life (since 2006).
 Developed some applications with my own interest.
                  */
+            };
+
+            HobbyProjects = new() {
+                new() {
+                    Title = "English to Bangla Machine Translator",
+                    StartDate = new DateTime(2012, 1, 1),
+                    EndDate = new DateTime(2014, 1, 1),
+                    UsedTechnologies = ".Net Framework 3.5, C#, MS SQL Server",
+                    Description = "A rule based english to bangla machine translator. It like a poor man's google translator.",
+                },
+                new() {
+                    Title = "Quran Study Tool",
+                    StartDate = new DateTime(2012, 1, 1),
+                    EndDate = new DateTime(2012, 12, 31),
+                    UsedTechnologies = ".Net Framework 3.5, C#, Access Database, HTML, CSS, JavaScript, and Web Browser control",
+                    Description = "This tool can be used to reading, listening, searching the Holy Quran in different languages (Arabic, Bengali, English).",
+                },
+                new() {
+                    Title = "Santali Keyboard Layout",
+                    StartDate = new DateTime(2012, 1, 1),
+                    EndDate = new DateTime(2012, 12, 31),
+                    UsedTechnologies = ".Net Framework 3.0, C#, Windows API",
+                    Description = "Using this tool one can type Santali (Santali is a language)",
+                },
+                new() {
+                    Title = "English, Bengali Dictionary",
+                    StartDate = new DateTime(2011, 1, 1),
+                    EndDate = new DateTime(2011, 12, 31),
+                    UsedTechnologies = ".Net Framework 3.0, C#, MS Access Database",
+                    Description = "Using this tool one can type Santali (Santali is a language)",
+                },
+                new() {
+                    Title = "Virus (clever 9) and Antivirus",
+                    StartDate = new DateTime(2009, 1, 1),
+                    EndDate = new DateTime(2009, 12, 31),
+                    UsedTechnologies = "Visual Basic 6",
+                    Description = "This program is able to spread out PC to PC via USB disk and delete your files " +
+                    "without your awareness. It can disable windows task manager, registry editor, and group policy etc. " +
+                    "To prevent this virus from attack an antivirus software has also been developed.",
+                },
+                new() {
+                    Title = "USB Disk Cleaner",
+                    StartDate = new DateTime(2008, 1, 1),
+                    EndDate = new DateTime(2009, 12, 31),
+                    UsedTechnologies = "Visual Basic 6",
+                    Description = "This prevents viruses to spread from USB Disk.",
+                },
+                new() {
+                    Title = "Bangla & English Speed Type",
+                    StartDate = new DateTime(2008, 1, 1),
+                    EndDate = new DateTime(2009, 12, 31),
+                    UsedTechnologies = "Visual Basic 6",
+                    Description = "This helps people to increase their Bangla and English typing speed.",
+                },
+                new() {
+                    Title = "PC Cleaner",
+                    StartDate = new DateTime(2007, 1, 1),
+                    EndDate = new DateTime(2009, 12, 31),
+                    UsedTechnologies = "Visual Basic 6",
+                    Description = "Clean up the temporary files that are no longer need.",
+                },
             };
 
             Awards = new() {

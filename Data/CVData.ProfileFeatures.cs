2e4083ef4b22ecd7668b5e439abd85c4746339f8
D Faruque Ahmad <dfaruque@gmail.com>
2023-04-03 16:35:16 +0600

    updated profile features

--- Data/CVData.ProfileFeatures.cs
+++ Data/CVData.ProfileFeatures.cs
@@ -11,21 +11,18 @@ public partial class CVData
     private static List<string> GetProfileFeatures()
     {
         return new() {
-            ExperienceYears + "+ years of professional software development experience",
-            ProgrammingExperienceYears + "+ years of programming experience",
-            "Knowledge of Object Oriented Programming (<b>OOP</b>)",
-            "Follows software design principles <b>SOLID</b>, <b>DRY</b>, and <b>KISS</b>",
-            "Strictly maintains coding conventions to write <b>clean code</b> and <b>maintainable software</b>",
-            "Good skills in analyzing requirements, designing, coding, and testing",
-            "Experience in <b>database design</b>, and <b>performance tuning</b>",
+            ExperienceYears + "+ years of professional software development experience and " + ProgrammingExperienceYears + "+ years of programming experience.",
+            "Strong foundation in Object Oriented Programming (<b>OOP</b>) and adherence to software design principles such as <b>SOLID</b>, <b>DRY</b>, and <b>KISS</b>, resulting in the creation of clean and efficient code that is easy to understand and modify.",
+            "Experienced in all phases of Software Development Life Cycle (<b>SDLC</b>) including analysis, design, development, testing and implementation of high quality & complex software.",
+            "Experience in <b>database design</b>, and <b>performance tuning</b>.",
             //"Works in various sizes of projects small to large scale",
-            "Knowledge in preventing various <b>web security</b> threats and attacks such as XSRF/CSRF, XSS, SQL Injection, DDoS attacks, etc.",
-            "Experienced in <b>RESTful API</b> and <b>JWT</b>",
-            "Knowledge of <b>image processing</b> and <b>computer vision</b>",
-            "Experience in <b>data extraction</b> and <b>transformation</b> from <b>Image</b>, <b>PDF</b>, and <b>Excel</b> files",
-            "Ability to jump into any problem and able to develop “<b>outside the box</b>” solutions",
+            "Knowledgeable in preventing <b>web security</b> threats and attacks such as XSRF/CSRF, XSS, SQL Injection, DDoS attacks, and more, ensuring that applications are secure and reliable.",
+            "Experienced in <b>RESTful API</b> and <b>JWT.</b>",
+            "Familiarity with <b>image processing</b> and <b>computer vision</b>, allowing for the creation of solutions that process visual data in innovative ways.",
+            "Capable of <b>extracting</b> and <b>transforming</b> data from various file types, including Image, PDF, and Excel, to streamline workflows and increase productivity.",
+            //"Ability to jump into any problem and able to develop “<b>outside the box</b>” solutions",
             //"Experience to work remote environment",
-            "Contributes to open-source projects. Notably Serenity platform",
+            "Contributes to <b>open-source</b> projects, with notable contributions to the <b>Serenity platform</b>, demonstrating dedication to advancing the field of software development.",
         };
     }
 }

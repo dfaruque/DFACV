b143e33333a73b96beb7230c4b8360c157e4048f
D Faruque Ahmad <dfaruque@gmail.com>
2023-04-03 15:22:26 +0600

    fixed tense

--- Data/CVData.cs
+++ Data/CVData.cs
@@ -24,7 +24,7 @@ public partial class CVData
             SocialMedias = GetSocialMedias(),
 
             Profile = "Strong planner and problem solver who readily adapts to change, works independently, and exceeds expectations. " +
-            "Begins programming in school life and dreams to become a Software Engineer.",
+            "Began programming in school and dreams of becoming a software engineer.",
 
             Features = GetProfileFeatures(),
 

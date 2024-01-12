2e1d77f1b0b14f994197e3b560fe28cf21c1d1d2
Deowan Faruque Ahamad <dfaruque@gmail.com>
2024-01-12 19:18:10 +0600

    changed bullets

--- Data/CVData.Experiences.cs
+++ Data/CVData.Experiences.cs
@@ -18,9 +18,9 @@ public partial class CVData
                 City = "Dhaka",
                 StartDate = new DateTime(2018, 1, 1),
                 EndDate = null,
-                Description = "<div>→ Working closely with project managers and clients.</div>" +
-                "<div>→ Collecting client requirements and converting them to technical solutions.</div>" +
-                "<div>→ As a development lead, help juniors understand and perform their tasks.</div>"
+                Description = "<div>• Working closely with project managers and clients from various bussiness domain.</div>" +
+                "<div>• Analyzed and translated complex client requirements into scalable technical solutions, resulting in a 40% reduction in development time and a 25% increase in customer satisfaction.</div>" +
+                "<div>• As a development lead, help juniors understand and perform their tasks.</div>"
             },
             new() {
                 Title = "Software Engineer",
@@ -29,9 +29,10 @@ public partial class CVData
                 City = "Dhaka",
                 StartDate = new DateTime(2016, 1, 1),
                 EndDate = new DateTime(2017, 12, 31),
-                Description = "<div>→ Worked closely with senior software engineers and analysts.</div>" +
-                "<div>→ Understood business and developed accordingly.</div>" +
-                "<div>→ Got a performance award!</div>"
+                Description = "<div>• Worked closely with senior software engineers and analysts.</div>" +
+                "<div>• Introduced a new framework (Serenity) in the company which reduces 50% developmnet time.</div>" +
+                "<div>• Understood business and developed accordingly.</div>" +
+                "<div>• Got a performance award!</div>"
             },
             new() {
                 Title = "Software Developer",
@@ -40,8 +41,8 @@ public partial class CVData
                 City = "Dhaka",
                 StartDate = new DateTime(2014, 11, 1),
                 EndDate = new DateTime(2015, 12, 31),
-                Description = "<div>→ Worked as a development team member of an ERP system.</div>" +
-                "<div>→ Understood business from analysts and developed accordingly.</div>"
+                Description = "<div>• Worked as a development team member of an ERP system.</div>" +
+                "<div>• Understood business from analysts and developed accordingly.</div>"
             },
         };
     }

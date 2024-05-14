fce6e650938598ed0a7a7644de66243144a6b44f
Deowan Faruque Ahamad <dfaruque@gmail.com>
2024-05-14 11:24:08 +0600

    updated to dotnet 8 project structure

--- Program.cs
+++ Program.cs
@@ -1,26 +1,11 @@
-using System;
-using System.Linq;
-using System.Net.Http;
 using DFACV;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//Added the condition that determine root components are already registered via prerednered HTML contents.
-if (!builder.RootComponents.Any())
-{
-    builder.RootComponents.Add<App>("#app");
-    builder.RootComponents.Add<HeadOutlet>("head::after");
-}
-
-ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
-
-static void ConfigureServices(IServiceCollection services, string baseAddress)
-{
-    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
-}
-
